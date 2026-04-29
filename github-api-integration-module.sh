31884882c10d34193f1685fc53c38c5d6ca4b675
Hareesh <your-email@example.com>
2026-04-29 06:05:27 +0000

    updated description for the shell

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -8,7 +8,7 @@
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
-#  i just use this for update description
+#  i just use this for update description in the shell
 #
 ################################
 
